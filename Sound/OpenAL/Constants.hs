313a55dbd2e919d30e52b242283c150e7a138704
Sven Panne <svenpanne@gmail.com>
2013-09-17 14:49:55 +0200

    Changed email address etc.

--- Sound/OpenAL/Constants.hs
+++ Sound/OpenAL/Constants.hs
@@ -2,10 +2,10 @@
 --------------------------------------------------------------------------------
 -- |
 -- Module      :  Sound.OpenAL.Constants
--- Copyright   :  (c) Sven Panne 2003-2009
--- License     :  BSD-style (see the file LICENSE)
+-- Copyright   :  (c) Sven Panne 2003-2013
+-- License     :  BSD3
 -- 
--- Maintainer  :  sven.panne@aedion.de
+-- Maintainer  :  Sven Panne <svenpanne@gmail.com>
 -- Stability   :  stable
 -- Portability :  portable
 --
